e01301a17aae699411bedfef66353f2d39355391
Patrick Faulkner <ptfaulkner@gmail.com>
2015-01-19 22:14:24 -0500

    Add one day's schedule

--- daStepper/daStepper.ino
+++ daStepper/daStepper.ino
@@ -6,21 +6,55 @@ Step stepper(servo);
 
 int minPosition = 54;
 int maxPosition = 126;
-int stepSize = 6;
 int servoPosition = minPosition;
-int timesToStep = 3500;
-int steps = 0;
+int oneMinute = 1000 * 60;
  
 void setup() 
 { 
   servo.attach(9);
+  
+  frequency();// 1
+  delay(oneMinute * 60);  //pause for one hour
+  frequency();// 2
+  delay(oneMinute * 60);  //pause for one hour
+  intensity();// 3
+  delay(oneMinute * 60);  //pause for one hour
+  frequency();// 4
+  delay(oneMinute * 5);
+  step(200, 4);
+  delay(oneMinute * 60);  //pause for one hour
+  frequency();// 5
+  delay(oneMinute * 30);
+  step(300, 4);
+  delay(oneMinute * 60);  //pause for one hour
+  frequency();// 6
+  delay(oneMinute * 80);
+  step(750, 4);
+  delay(oneMinute * 45);
+  step(350, 4);
+  delay(oneMinute * 30);
+  step(2000, * 3);
 } 
  
 void loop() 
 { 
-  if(steps < timesToStep) 
+
+}
+
+step(int steps, int stepSize) 
+{
+  for(int i = 0; i < steps; i++)
   {
-     stepper.step(minPosition, maxPosition, stepSize);
-     steps++;
+    stepper.step(minPosition, maxPosition, stepSize);
   }
+}
+
+void frequency()
+{
+  step(500, 4);
+}
+
+void intensity()
+{
+  step(3500, 6);
 }
